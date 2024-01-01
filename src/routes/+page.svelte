1a4317a602d4156e45b3180d40012346252ebcab
Khai Wei Ong <ongkw@sg.ibm.com>
2024-01-01 18:55:16 +0800

    first draft

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<h1>Welcome to IBM Carbon Portal Example</h1>
+<p>Visit <a href="https://svelte.carbondesignsystem.com/">https://svelte.carbondesignsystem.com/</a> to read the documentation</p>
+<p>Developed by Ong Khai Wei</p>
